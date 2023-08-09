929743b16bf307a08e643067b72881ca409b738f
Yadvendra Sharma <yadvendras20@gmail.com>
2023-08-09 17:45:20 +0530

    m

--- index.js
+++ index.js
@@ -15,18 +15,22 @@ app.use(cors());
 
 
   //Routes
-  app.post("/nav",async(req,res)=>{
+  app.get("/nav",async(req,res)=>{
     
-const {key,secret}=req.body
+// const {key,secret}=req.body
+const key="7JL6ho3WoVXq2T6rHRx7FUvQciAJgA"
+const secret= "7hmBQDj6wH24ro1ktMu636g3Y8dx7Em93als99jq4HH2g58VQBIKFXqQKLCO"
+const initial_wallet=100 //can be changed accordingly
+
 const DeltaRestClient = require("delta-rest-client");
-new DeltaRestClient(key,secret).then(client => {
+new DeltaRestClient("7JL6ho3WoVXq2T6rHRx7FUvQciAJgA","7hmBQDj6wH24ro1ktMu636g3Y8dx7Em93als99jq4HH2g58VQBIKFXqQKLCO").then(client => {
     client.apis.Wallet.getBalances()
     .then(function(response) {
     //   console.log(response.body["result"]);
       const resp=response.body["result"].filter(item=>{
         if(item["asset_id"]==5){
             console.log(item.balance)
-            res.send(item.balance)
+            res.send((item.balance/initial_wallet)*100)
         }
       })
     }).catch(function(e) {
